e49594ce95976026d8865c42ca376b9319df1a0a
Yosef Khayat <yousef.khayat24@gmail.com>
2018-01-02 17:20:26 +0200

    Update CreateGrid comments

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 
-
+//this function create Grid for the game, the grid is built from GirdSize Rows and in each Row there is GirdSize columns
+//the new created Rows has id row[i], for example the first row has id row0, and class row
+//the new created Columns has class column
 function CreateGrid(GridSize) {
-	//for visual porposes to command line where add to demonstrate the grid is build up well
-	//$("#app-container").css({"width":"600px","height":"600px","background-color":"red", "display": "table","border-collapse":"collapse"});
-	//var columnWidth=100/GridSize;
 	for(var i=0;i<GridSize;i++){
 		var rowName='<div id="row'+i+'" class="row"></div>';
 		$("#app-container").append(rowName);
