79fc57db626252e45cb3403fec705e5e111a607b
dor4231 <dor4231@gmail.com>
2018-01-28 08:36:38 +0200

    Added - Timer functionality, Document.ready(). Improved - Player area styles.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-// Definition of global variables
+// Global Variables Definition
 
 var currentPlayer;	// can be 1 or 2
-var audio = {}
+var audio = {};
 
+// Sound JSON.
 audio["tic"] = new Audio();
 audio["tic"].src = "assets/sounds/Tick-DeepFrozenApps-397275646.mp3"
 
-//this function create Grid for the game, the grid is built from GirdSize Rows and in each Row there is GirdSize columns
-//the new created Rows has id row[i], for example the first row has id row0, and class row
-//the new created Columns has class column
+//	Creates a Grid for the game, the grid is built from
+// an array of divs represents the grid blocks.
+// 
+// the new created Columns has class column
 function CreateGrid(GridSize) {
 	for(var i=0;i<GridSize*GridSize;i++){
 		$('#app-container').append('<div class="column unchecked"></div>');
@@ -25,8 +27,57 @@ function StartGame(GridSize,GameMode,LengthOfTurn){
 	$('#console-container').show();
 	CreateGrid(GridSize);
 	currentPlayer = 1;
-	setTimer(10);
+	setTimer(5,1);
+};
 
+// This function set the current player
+
+function nextPlayer() {
+	if (currentPlayer === 1) {
+		currentPlayer = 2;
+		setTimer(5,2);
+	}
+	else {
+		currentPlayer = 1;
+		setTimer(5,1);
+	}
+};
+
+// This function set the timer
+
+function setTimer(interval,player){
+	var stopTime = interval;
+	var playerNum = player - 1;
+	var x = setInterval(function(){
+
+		if (stopTime <= 0 ) {
+			clearInterval(x);
+			$('.timer')[playerNum].innerHTML = "Time out";
+// 			playRandom();
+			nextPlayer();
+		}
+		else
+		{
+			$('.timer')[playerNum].innerHTML = stopTime--;
+			console.log(stopTime);
+		}
+	},1000);
+};
+
+// This function play random move when time out
+function randomMove() {
+	
+};
+
+
+
+$(document).ready(function() {
+	console.log("ready!");
+	$("#start").click(function() {
+		console.log("WOW");
+		StartGame(3);
+	});
+	
 	$(".unchecked").click(function(){
 		if( $(this).hasClass("unchecked")) {
 			$(this).removeClass("unchecked");
@@ -36,47 +87,4 @@ function StartGame(GridSize,GameMode,LengthOfTurn){
 		}
 
 	});
-
-};
-
-// This function set the current player
-
-function nextPlayer() {
-	if (currentPlayer === 1) {
-		currentPlayer = 2;
-	}
-	else {
-		currentPlayer = 1;
-	}
-};
-
-// This function set the timer
-
-function setTimer(interval){
-	var stopTime = interval;
-	var x = setInterval(function(){
-
-		if (stopTime <= 0 ) {
-			clearInterval(x);
-			$('.playerTimer')[0].innerHTML = "Time out";
-			playRandom();
-		}
-		else
-		{
-			$('.playerTimer')[0].innerHTML = stopTime--;
-		}
-	},1000);
-};
-
-// This function play random move when time out
-
-function playRandom() {
-
-};
-
-
-$("#start").click(function() {
-
-	console.log("WOW");
 });
-
